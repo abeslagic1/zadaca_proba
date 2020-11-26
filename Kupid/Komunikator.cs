7db7b3371209df31763bb3e8c783531febbc847a
MedinaKapo <mkapo2@etf.unsa.ba>
2020-11-26 19:15:21 +0100

    Add project files.

---
+++ Kupid/Komunikator.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kupid
+{
+    public class Komunikator
+    {
+        #region Atributi
+
+        List<Korisnik> korisnici;
+        List<Chat> razgovori;
+
+        #endregion
+
+        #region Properties
+
+        public List<Korisnik> Korisnici
+        {
+            get => korisnici;
+        }
+
+        public List<Chat> Razgovori
+        {
+            get => razgovori;
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Komunikator()
+        {
+            korisnici = new List<Korisnik>();
+            razgovori = new List<Chat>();
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void RadSaKorisnikom(Korisnik k, int opcija)
+        {
+            if (opcija == 0)
+            {
+                Korisnik postojeci = korisnici.Find(korisnik => korisnik.Ime == k.Ime);
+                if (postojeci != null)
+                    throw new InvalidOperationException("Korisnik već postoji!");
+
+                korisnici.Add(k);
+            }
+            else if (opcija == 1)
+            {
+                Korisnik postojeci = korisnici.Find(korisnik => korisnik.Ime == k.Ime);
+                if (postojeci == null)
+                    throw new InvalidOperationException("Korisnik ne postoji!");
+
+                korisnici.Remove(k);
+
+                List<Chat> razgovoriZaBrisanje = new List<Chat>();
+                foreach (Chat c in razgovori)
+                {
+                    if (c.Korisnici.Find(korisnik => korisnik.Ime == k.Ime) != null)
+                        razgovoriZaBrisanje.Add(c);
+                }
+
+                foreach (Chat brisanje in razgovoriZaBrisanje)
+                    razgovori.Remove(brisanje);
+            }
+        }
+
+        public void DodavanjeRazgovora(List<Korisnik> korisnici, bool grupniChat)
+        {
+            if (korisnici == null || korisnici.Count < 2 || (!grupniChat && korisnici.Count > 2))
+                throw new ArgumentException("Nemoguće dodati razgovor!");
+
+            if (grupniChat)
+                razgovori.Add(new GrupniChat(korisnici));
+
+            else
+                razgovori.Add(new Chat(korisnici[0], korisnici[1]));
+        }
+
+        /// <summary>
+        /// Metoda u kojoj se vrši pronalazak svih poruka koje u sebi sadrže traženi sadržaj.
+        /// Ukoliko je sadržaj prazan ili ne postoji nijedan chat, baca se izuzetak.
+        /// U razmatranje se uzimaju i grupni, i individualni chatovi, a ne smije se uzeti u razmatranje
+        /// nijedan chat u kojem se nalazi korisnik sa imenom "admin".
+        /// </summary>
+        public List<Poruka> IzlistavanjeSvihPorukaSaSadržajem(string sadržaj)
+        {
+            List<Poruka> vracam=new List<Poruka>();
+            for(int i = 0; i < razgovori.Count; i++)
+            {
+                if (razgovori[i].Korisnici.Equals("admin")) continue;
+                else
+                {
+                    List<Poruka> lokalne=razgovori[i].Poruke;
+                    for(int j = 0; j < lokalne.Count; j++)
+                    {
+                        if (lokalne[i].Sadrzaj.Contains(sadržaj) && !lokalne[i].Posiljalac.Ime.Equals("admin") &&
+                            !lokalne[i].Primalac.Ime.Equals("admin"))
+                        {
+                            vracam.Add(lokalne[i]);
+                        }
+                    }
+                    if (razgovori.Count == 0) throw new ArgumentNullException("nema");
+                    if (vracam.Count == 0) throw new ArgumentNullException("nema");
+                }
+            }
+            return vracam;
+        }
+
+        public bool DaLiJeSpajanjeUspjesno(Chat c, IRecenzija r)
+        {
+            if (c is GrupniChat)
+                throw new InvalidOperationException("Grupni chatovi nisu podržani!");
+
+            if (c.Poruke.Find(poruka => poruka.IzračunajKompatibilnostKorisnika() == 100) != null
+                && r.DajUtisak() == "Pozitivan")
+                return true;
+
+            else
+                return false;
+        }
+
+        public void SpajanjeKorisnika()
+        {
+            Korisnik k1;
+            if (korisnici.Count != 0)
+                k1 = korisnici[0];
+            else k1 = null;
+            Korisnik k2;
+            for(int i = 1; i < korisnici.Count; i++)
+            {
+                k2 = korisnici[i];
+                if ((k1.Lokacija == k2.Lokacija && k1.ZeljenaLokacija == k2.ZeljenaLokacija && k1.Godine == k2.Godine) ||
+                    (k1.Lokacija==k2.Lokacija && k1.ZeljenaLokacija==k2.ZeljenaLokacija) || (k1.Lokacija==k2.Lokacija && 
+                    k1.Godine==k2.Godine)){
+                    Chat novi = new Chat(k1, k2);
+                    razgovori.Add(novi);
+                        }
+                else
+                {
+                    throw new ArgumentException("greska");
+                }
+                
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Kupid/Poruka.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Kupid
+{
+    public class Poruka
+    {
+        #region Atributi
+
+        Korisnik posiljalac, primalac;
+        string sadrzaj;
+
+        #endregion
+
+        #region Properties
+
+        public Korisnik Posiljalac
+        {
+            get => posiljalac;
+        }
+        public Korisnik Primalac
+        {
+            get => primalac;
+        }
+        public string Sadrzaj
+        {
+            get => sadrzaj;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Contains("pogrdna riječ"))
+                    throw new InvalidOperationException("Neispravan sadržaj poruke!");
+
+                sadrzaj = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Poruka(Korisnik sender, Korisnik receiver, string content)
+        {
+            if (sender == null || receiver == null)
+                throw new ArgumentNullException("Nedefinisan pošiljalac/primalac!");
+
+            posiljalac = sender;
+            primalac = receiver;
+            Sadrzaj = content;
+        }
+
+        #endregion
+
+        #region Metode
+
+        /// <summary>
+        /// Metoda u kojoj se izračunava kompatibilnost korisnika.
+        /// Ako su lokacije, broj godina, minimalni i maksimalni željeni broj godina korisnika isti, kompatibilnost je 100%.
+        /// Kompatibilnost je 100% bez obzira na parametre ukoliko se u sadržaju poruke nalazi string "volim te".
+        /// Ako se tri parametra podudaraju, kompatibilnost je 75%, ako se podudaraju dva onda je 50%,
+        /// ako se podudara jedan, onda je 25% a u suprotnom je 0%.
+        /// </summary>
+        /// <returns></returns>
+        public double IzračunajKompatibilnostKorisnika()
+        {
+            double kompatiblinost = 0;
+            int brojKompatibilnosti = 0;
+            if (posiljalac.ZeljeniMaxGodina == primalac.ZeljeniMaxGodina) brojKompatibilnosti++;
+            if (posiljalac.Lokacija == primalac.Lokacija) brojKompatibilnosti++;
+            if (posiljalac.ZeljeniMinGodina == primalac.ZeljeniMinGodina) brojKompatibilnosti++;
+            if (posiljalac.Godine == primalac.Godine) brojKompatibilnosti++;
+            else if (sadrzaj.Contains("volim te")) { kompatiblinost = 100; brojKompatibilnosti = 4; }
+            if (brojKompatibilnosti == 4) kompatiblinost = 100;
+            else if (brojKompatibilnosti == 3) kompatiblinost = 75;
+            else if (brojKompatibilnosti == 2) kompatiblinost = 50;
+            else if (brojKompatibilnosti == 1) kompatiblinost = 25;
+            else kompatiblinost = 0;
+
+            return kompatiblinost;
+
+        }
+
+        #endregion
+    }
+}
